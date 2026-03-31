a9f4cbcdb86bfd032383318e048c9384b18088e3
Sidharth Swain <sidharthswain2005@gmail.com>
2026-03-31 12:23:16 -0400

    Unit Test performed

---
+++ test_emotion_detection.py
@@ -0,0 +1,17 @@
+from EmotionDetection.emotion_detection import emotion_detector
+import unittest
+class TestEmotionDetection(unittest.TestCase):
+    def test_emotion_detection(self):
+        test_cases = [
+            ("I am glad this happened", "joy"),
+            ("I am really mad about this", "anger"),
+            ("I feel disgusted just hearing about this", "disgust"),
+            ("I am so sad about this", "sadness"),
+            ("I am really afraid that this will happen", "fear")
+        ]
+
+        for statement, expected_emotion in test_cases:
+            result = emotion_detector(statement)
+            self.assertEqual(result['dominant_emotion'], expected_emotion)
+
+unittest.main()
